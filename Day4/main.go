7dd302aaa1a42d905679236aaf530db55c2f0be8
Suliaman Abass <suliaman.abass46@gmail.com>
2024-08-17 23:37:20 +0530

    Day4 Part 2

--- Day4/main.go
+++ Day4/main.go
@@ -10,6 +10,7 @@ import (
 )
 
 type card struct {
+	id             int
 	winningNumbers []int
 	cardNumbers    []int
 }
@@ -23,6 +24,13 @@ func main() {
 	defer file.Close()
 
 	partOne(file)
+
+	_, err = file.Seek(0, 0)
+	if err != nil {
+		return
+	}
+
+	partTwo(file)
 }
 
 func partOne(file *os.File) {
@@ -60,8 +68,28 @@ func intersectNumbers(card card) []int {
 }
 
 func parseCard(line []byte) card {
+	id := resolveCardId(line)
 	pipeIndex := bytes.IndexByte(line, 124)
-	return card{resolveWinningNumbers(line, pipeIndex), resolveCardNumbers(line, pipeIndex)}
+	return card{id, resolveWinningNumbers(line, pipeIndex), resolveCardNumbers(line, pipeIndex)}
+}
+
+func resolveCardId(line []byte) int {
+	// id is immediately before the colon
+	idOnesPlaceIdx := bytes.IndexByte(line, 58) - 1
+	idLen := idOnesPlaceIdx - 4
+
+	var id = 0
+	for i := 0; i < idLen; i++ {
+		id *= 10
+
+		elm := line[5+i]
+		if elm == 32 {
+			continue
+		}
+
+		id += int(elm - 48)
+	}
+	return id
 }
 
 func resolveCardNumbers(line []byte, pipeIndex int) []int {
@@ -107,4 +135,52 @@ func resolveTwoDigitNumber(line []byte, idx int) int {
 	number, _ := strconv.Atoi(num)
 
 	return number
+}
+
+func partTwo(file *os.File) {
+	cardTally := map[int]int{}
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		card := parseCard(scanner.Bytes())
+		intersection := intersectNumbers(card)
+		addToTally(cardTally, card.id, len(intersection))
+	}
+
+	total := 0
+	for _, val := range cardTally {
+		total += val
+	}
+
+	fmt.Println("Total Part Two: ", total)
+
+}
+
+func addToTally(cardTally map[int]int, cardId int, intersectionLength int) {
+	cur, exists := cardTally[cardId]
+	if exists {
+		handleCardCopies(cardTally, cardId, intersectionLength, cur)
+	}
+
+	for i := cardId; i <= cardId+intersectionLength; i++ {
+		addOrUpdateCardCount(cardTally, i)
+	}
+}
+
+func handleCardCopies(cardTally map[int]int, cardId int, intersectionLength int, cur int) {
+	for j := 0; j < cur; j++ {
+		for i := cardId + 1; i <= cardId+intersectionLength; i++ {
+			addOrUpdateCardCount(cardTally, i)
+		}
+	}
+}
+
+func addOrUpdateCardCount(cardTally map[int]int, cardId int) {
+	cur, exists := cardTally[cardId]
+	if exists {
+		cardTally[cardId] = cur + 1
+		return
+	}
+	cardTally[cardId] = 1
 }
