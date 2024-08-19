95d2ebbcffb1dbc7725bddca3cf6b1af54d4e373
Suliaman Abass <suliaman.abass46@gmail.com>
2024-08-20 00:22:47 +0530

    Day6 Part 2

--- Day6/main.go
+++ Day6/main.go
@@ -4,7 +4,9 @@ import (
 	"aoc23/utils"
 	"bufio"
 	"fmt"
+	"math"
 	"os"
+	"time"
 )
 
 type Race struct {
@@ -21,13 +23,36 @@ func main() {
 	defer file.Close()
 
 	partOne(file)
+
+	utils.SeekToFileStart(file)
+
+	start := time.Now()
+
+	partTwo(file, findTotal)
+	
+	elapsed := time.Since(start)
+	fmt.Println("Part Two by loop solution took: ", elapsed)
+
+	start = time.Now()
+
+	partTwo(file, findTotalByQuadratic)
+
+	elapsed = time.Since(start)
+	fmt.Println("Part Two by quadratic solution took: ", elapsed)
 }
 
 func partOne(file *os.File) {
-	races := parseFile(file)
+	races := parseFile(file, parseLineIntoSeparateRaces)
 	total := findTotal(races)
 
 	fmt.Println("Total Part One: ", total)
+}
+
+func partTwo(file *os.File, strategy func([]Race) int) {
+	races := parseFile(file, parseLineIntoSingleRace)
+	total := strategy(races)
+
+	fmt.Println("Total Part Two: ", total)
 }
 
 func findTotal(races []Race) int {
@@ -56,21 +81,50 @@ func findTotal(races []Race) int {
 	return total
 }
 
+func findTotalByQuadratic(races []Race) int {
+	var solStart int
+	var solEnd int
+
+	var total = 1
+
+	for _, race := range races {
+		startFloat := 0.5 * (float64(race.time) - math.Sqrt(math.Pow(float64(race.time), 2)-float64(4*race.distance)))
+		endFloat := 0.5 * (float64(race.time) + math.Sqrt(math.Pow(float64(race.time), 2)-float64(4*race.distance)))
+
+		solStart = handleQuadraticRounding(startFloat, 1)
+		solEnd = handleQuadraticRounding(endFloat, -1)
+
+		total *= solEnd - solStart + 1
+	}
+
+	return total
+}
+
+func handleQuadraticRounding(float float64, round float64) int {
+	if float == math.Trunc(float) {
+		return int(float + round)
+	}
+	if round == 1 {
+		return int(math.Ceil(float))
+	}
+	return int(math.Floor(float))
+}
+
 func isRecordBroken(i int, race Race) bool {
 	return i*((race.time)-i) > race.distance
 }
 
-func parseFile(file *os.File) []Race {
+func parseFile(file *os.File, lineParser func([]byte, *[]int)) []Race {
 	scanner := bufio.NewScanner(file)
 	var num []int
 	for scanner.Scan() {
 		line := scanner.Bytes()
-		parseLine(line, &num)
+		lineParser(line, &num)
 	}
-	return parseRaces(num)
+	return convertToRaces(num)
 }
 
-func parseLine(line []byte, nums *[]int) {
+func parseLineIntoSeparateRaces(line []byte, nums *[]int) {
 	var firstNumIdx int
 	for i, char := range line {
 		if utils.IsDigit(char) {
@@ -95,7 +149,27 @@ func parseLine(line []byte, nums *[]int) {
 	*nums = append(*nums, num)
 }
 
-func parseRaces(nums []int) []Race {
+func parseLineIntoSingleRace(line []byte, nums *[]int) {
+	var firstNumIdx int
+	for i, char := range line {
+		if utils.IsDigit(char) {
+			firstNumIdx = i
+			break
+		}
+	}
+
+	num := 0
+	for i := firstNumIdx; i < len(line); i++ {
+		if utils.IsDigit(line[i]) {
+			num *= 10
+			num += int(line[i] - 48)
+		}
+	}
+
+	*nums = append(*nums, num)
+}
+
+func convertToRaces(nums []int) []Race {
 	var races []Race
 	split := len(nums) / 2
 
