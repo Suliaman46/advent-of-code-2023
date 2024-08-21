92e2c67b63abbac7e2542bf81ef2b253125f400b
Suliaman Abass <suliaman.abass46@gmail.com>
2024-08-21 15:13:31 +0530

    Day8 Part 1: Complete

--- Day8/main.go
+++ Day8/main.go
@@ -13,6 +13,16 @@ type Node struct {
 	right *Node
 }
 
+type DesertMap struct {
+	instructions []string
+	startingNode *Node
+}
+
+const StartPosition = "AAA"
+const EndPosition = "ZZZ"
+const LeftStep = "L"
+const RightStep = "R"
+
 func main() {
 	file, err := os.Open("./input.txt")
 	if err != nil {
@@ -25,17 +35,28 @@ func main() {
 }
 
 func partOne(file *os.File) {
-	parseTree(file)
+	desertMap := parseTree(file)
+	steps := walkMap(desertMap)
+	fmt.Println("Total Steps Part One: ", steps)
 }
 
-func parseTree(file *os.File) *Node {
+func parseTree(file *os.File) DesertMap {
 	var head *Node
 
 	var seenNodes = make(map[string]*Node)
 
 	scanner := bufio.NewScanner(file)
+	idx := 0
+	var instructions []string
 	for scanner.Scan() {
 		line := scanner.Text()
+
+		if idx == 0 {
+			for _, char := range line {
+				instructions = append(instructions, string(char))
+			}
+			idx++
+		}
 
 		if !strings.Contains(line, "=") {
 			continue
@@ -58,19 +79,13 @@ func parseTree(file *os.File) *Node {
 
 			seenNodes[pos] = &curr
 
-			if pos == "AAA" {
+			if pos == StartPosition {
 				head = &curr
 			}
 		}
 	}
 
-	fmt.Println("Head", head)
-	for pos, node := range seenNodes {
-		if node.right == nil || node.right == nil {
-			fmt.Println("Node with nil children at: ", pos)
-		}
-	}
-	return head
+	return DesertMap{instructions, head}
 }
 
 func handleSeenNode(node *Node, posLeft string, posRight string, seenNodes *map[string]*Node) {
@@ -126,4 +141,33 @@ func handleRightChild(node *Node, posRight string, seenNodes *map[string]*Node) 
 	newChild := Node{posRight, nil, nil}
 	node.right = &newChild
 	(*seenNodes)[posRight] = &newChild
+}
+
+func walkMap(desertMap DesertMap) int {
+	var curr *Node
+	curr = desertMap.startingNode
+	steps := 0
+	for i := 0; i < len(desertMap.instructions); i++ {
+		instruction := desertMap.instructions[i]
+
+		if curr.pos == EndPosition {
+			return steps
+		}
+
+		// Reached end of instructions but not ZZZ so restart
+		if i == len(desertMap.instructions)-1 {
+			i = -1
+		}
+
+		switch instruction {
+		case LeftStep:
+			curr = curr.left
+		case RightStep:
+			curr = curr.right
+		}
+
+		steps++
+	}
+
+	return steps
 }
